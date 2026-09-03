262afa4194a7bd23328679fb24264174f2ea85a6
Harishragavendran07 <sudhanharish1999@gmail.com>
2026-09-03 20:34:07 +0530

    Fix PostgreSQL SSL connection

--- express-backend/src/config/db.js
+++ express-backend/src/config/db.js
@@ -5,10 +5,13 @@ dotenv.config();
 
 const pool = new Pool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: Number(process.env.DB_PORT),
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD
+  password: process.env.DB_PASSWORD,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
